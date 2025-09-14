733498fcd4d55547e3d46ecd240c600dbfe6551b
luutanhung <luutanhung.dev@gmail.com>
2025-09-14 17:07:41 +0700

    add custom redirection when running in production

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+import sys
+
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from slowapi.errors import RateLimitExceeded
 
 from app.exceptions import NotFound, not_found_handler
@@ -7,7 +10,12 @@ from app.core.rate_limiter import limiter, rate_limit_exceeded_handler
 from app.routers import health_router
 from app.quotes.router import router as quote_router
 
-app = FastAPI()
+IS_PROD = "production" if sys.argv[1] == "run" else "development"
+
+if IS_PROD == "production":
+    app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+else:
+    app = FastAPI()
 
 app.state.limiter = limiter
 
@@ -15,6 +23,28 @@ app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
 app.add_exception_handler(NotFound, not_found_handler)
 
 app.add_middleware(LoggingMiddleware)
+
+
+@app.get("/", include_in_schema=False)
+async def homepage():
+    if IS_PROD:
+        return RedirectResponse("https://luutanhung.github.io/finance-quote-api/")
+    RedirectResponse("/docs")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_docs():
+    if IS_PROD:
+        return RedirectResponse("https://luutanhung.github.io/finance-quote-api/")
+    RedirectResponse("/docs")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_redoc():
+    if IS_PROD:
+        return RedirectResponse("https://luutanhung.github.io/finance-quote-api/")
+    RedirectResponse("/redoc")
+
 
 app.include_router(health_router)
 app.include_router(quote_router)
